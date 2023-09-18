e586ab6e8f61fa934bd1657f26eabb97796f241b
jynkeshie <jynkeshie@gmail.com>
2023-09-18 16:01:03 -0700

    Updated main.h file prototype for Task 0

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_string(const char *str);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_unsign(unsigned int n, int base, const char *digits);
+int print_binary(unsigned int n);
+int _printf_binary(const char *format, ...);
+
+#endif /* MAIN_H */
