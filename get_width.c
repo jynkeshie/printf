a3ba9b1a8ef18b085a580fab2a8f32e7ecb20d3f
Mutai Kelvin <Mutai05>
2023-09-19 13:18:59 -0700

    Updated get_width.c file

--- get_width.c
+++ get_width.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Calculates width for printing.
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: width.
  */
+
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
