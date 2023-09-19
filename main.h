f99dd9f2e91c1392e7fc31e0b3022c12345bd70d
Mutai Kelvin <Mutai05>
2023-09-19 06:31:13 -0700

    Created _putchar.c, main.h, main.c and _putchar.c files Written code for _printf.c file Written code for main.h file Written code for _putchar.c file Used main.c test file provided for the task

--- main.h
+++ main.h
@@ -1,11 +1,46 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PRINT_F
+#define PRINT_F
 
-void print_string(const char *str);
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_unsign(unsigned int n, int base, const char *digits);
-int print_binary(unsigned int n);
-int _printf_binary(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
-#endif /* MAIN_H */
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
+
+#endif
